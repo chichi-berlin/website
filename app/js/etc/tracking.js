cb7ea1b27b711d20c84a57c96d9c71779eefa33f
Lucendio <dev@lucend.io>
2021-05-09 23:36:05 +0200

    [fix] Fix tracking opt out (move from piwik to matomo)
    * changes on the piwik backend required these changes

--- app/js/etc/tracking.js
+++ app/js/etc/tracking.js
@@ -1,5 +1,5 @@
 var _paq = _paq || [];
-_paq.push(['setTrackerUrl', 'https://onwx.org/piwik/piwik.php']);
+_paq.push(['setTrackerUrl', 'https://matomo.onwx.org/piwik.php']);
 _paq.push(['setSiteId', '6']);
 _paq.push(["setDomains", ["*.chichi.berlin"]]);
 _paq.push(["setDoNotTrack", true]);
